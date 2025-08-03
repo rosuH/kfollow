0a869c2718304e00d1cd1873f980bff5771bbe79
rosuH <hi@rosuh.me>
2025-08-03 21:43:16 +0800

    feat(webview): enable DOM storage and cookies in WebViewConfig
    Added configuration for enabling DOM storage and accepting both general and third-party cookies using CookieManager.
    
    > GENERATE BY https://aicommit.app

--- composeApp/src/androidMain/kotlin/me/rosuh/WebViewConfig.kt
+++ composeApp/src/androidMain/kotlin/me/rosuh/WebViewConfig.kt
@@ -1,9 +1,14 @@
 package me.rosuh
 
+import android.webkit.CookieManager
 import android.webkit.WebView
 
 fun WebView.applyConfig() {
     settings.apply {
+        domStorageEnabled = true
         javaScriptEnabled = true
     }
+    val cookieManager = CookieManager.getInstance()
+    cookieManager.setAcceptCookie(true) // General cookie acceptance
+    cookieManager.setAcceptThirdPartyCookies(this, true)
 }
